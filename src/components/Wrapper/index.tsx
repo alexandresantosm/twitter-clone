812a163793424cbf856a98a4de598895d02c0a73
Alexandre Menezes <amenezes031@gmail.com>
2021-07-10 16:50:32 -0300

    add wrapper component

---
+++ src/components/Wrapper/index.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+import { Container } from './styles';
+
+interface WrapperProps {
+  children: ReactNode;
+}
+
+export function Wrapper({ children }: WrapperProps) {
+  return <Container>{children}</Container>;
+}
---
+++ src/components/Wrapper/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100%;
+  max-width: 1170px;
+  margin: 0 auto;
+  padding: 0 30px;
+`;
