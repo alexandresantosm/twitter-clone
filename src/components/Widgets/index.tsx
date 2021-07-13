1639dbfd7d35cf2ab4ab702f694c66aa161e134a
Alexandre Menezes <amenezes031@gmail.com>
2021-07-13 18:02:54 -0300

    add follows and trends components in the widgets

--- src/components/Widgets/index.tsx
+++ src/components/Widgets/index.tsx
@@ -1,5 +1,26 @@
+import { Follows } from './Follows';
 import { Container } from './styles';
+import { Trends } from './Trends';
 
 export function Widgets() {
-  return <Container>Widgets</Container>;
+  return (
+    <Container>
+      <Follows />
+
+      <Trends />
+
+      <footer>
+        <div>
+          @ 2018 Twitter <a href="">À propos</a>
+        </div>
+        <div>
+          <a href="">Centro d&lsquo;assistence</a> <a href="">Conditions</a>
+        </div>
+        <div>
+          <a href="">Politique deconfidentialité</a> <a href="">Cookies</a>
+        </div>
+        <a href="">Informations sur la publicité</a>
+      </footer>
+    </Container>
+  );
 }
--- src/components/Widgets/styles.ts
+++ src/components/Widgets/styles.ts
@@ -1,3 +1,18 @@
 import styled from 'styled-components';
 
-export const Container = styled.aside``;
+export const Container = styled.aside`
+  margin-top: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  footer {
+    div,
+    a {
+      color: var(--gray-600);
+      font-size: 0.75rem;
+      text-decoration: none;
+    }
+  }
+`;
