642894b57d01cce60b2bdcdd2997ed5f4514ae2a
Alexandre Menezes <amenezes031@gmail.com>
2021-07-13 22:30:45 -0300

    finished timeline

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,5 +1,164 @@
-import { Container } from './styles';
+import { TimelineNav } from './TimelineNav';
+
+import {
+  Container,
+  Content,
+  TweetList,
+  TweetItem,
+  Avatar,
+  Info,
+  Actions,
+} from './styles';
 
 export function Timeline() {
-  return <Container>Timeline</Container>;
+  return (
+    <Container>
+      <Content>
+        <TimelineNav />
+
+        <TweetList>
+          <TweetItem>
+            <Avatar src="images/avatar.png" alt="Avatar" />
+            <Info>
+              <strong>
+                Benoît Vrins <span>@Exibit</span> <span>26 janv.</span>
+              </strong>
+              <p>
+                I just published &quot;The Belgian Red Cross website : backstage
+                of a revamp like no other&quot;
+              </p>
+
+              <a href="#">translated from French</a>
+
+              <div className="image">
+                <img
+                  src="https://images.unsplash.com/photo-1626180583122-c256e0ea54b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+                  alt="Image"
+                />
+                <div className="legend">
+                  <strong>
+                    The Belgian Red Cross website : backstage of a revamp like
+                    no other
+                  </strong>
+                  <p>
+                    It&lsquo;s a pleasure for the whole team to announce today
+                    the release of the new Belgian Red Cross website. It was an
+                    opportunity for Spade to
+                  </p>
+                  <span>bordet.be</span>
+                </div>
+              </div>
+
+              <Actions>
+                <a href="">
+                  <img src="images/comments.svg" alt="Comments" />
+                  <span>3</span>
+                </a>
+                <a href="">
+                  <img src="images/retweet.svg" alt="Retweet" />
+                  <span>6</span>
+                </a>
+                <a href="">
+                  <img src="images/like.svg" alt="Like" />
+                  <span>2</span>
+                </a>
+                <a href="">
+                  <img src="images/message.svg" alt="Message" />
+                  <span>12</span>
+                </a>
+              </Actions>
+            </Info>
+          </TweetItem>
+          <TweetItem>
+            <Avatar src="images/avatar.png" alt="Avatar" />
+            <Info>
+              <div className="retweet">
+                <img src="images/retweet.svg" alt="Retweet" />
+                <span>Benoît Vrins has retweeted</span>
+              </div>
+              <strong>
+                Benoît Vrins <span>@Exibit</span> <span>26 janv.</span>
+              </strong>
+              <p>
+                app <a href="#">@itsmeDigitalID</a> désinstallée puis
+                réinstallée, comment faire pour retrouver son compte ? Ma banque
+                dit que mon token existe déja #UX
+              </p>
+
+              <Actions>
+                <a href="">
+                  <img src="images/comments.svg" alt="Comments" />
+                  <span>3</span>
+                </a>
+                <a href="">
+                  <img src="images/retweet.svg" alt="Retweet" />
+                  <span>6</span>
+                </a>
+                <a href="">
+                  <img src="images/like.svg" alt="Like" />
+                  <span>2</span>
+                </a>
+                <a href="">
+                  <img src="images/message.svg" alt="Message" />
+                  <span>12</span>
+                </a>
+              </Actions>
+            </Info>
+          </TweetItem>
+          <TweetItem>
+            <Avatar src="images/avatar.png" alt="Avatar" />
+            <Info>
+              <strong>
+                Benoît Vrins <span>@Exibit</span> <span>26 janv.</span>
+              </strong>
+              <p>
+                I just published &quot;The Belgian Red Cross website : backstage
+                of a revamp like no other&quot;
+              </p>
+
+              <a href="#">translated from French</a>
+
+              <div className="image">
+                <img
+                  src="https://images.unsplash.com/photo-1553272725-086100aecf5e?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
+                  alt="Image"
+                />
+                <div className="legend">
+                  <strong>
+                    The Belgian Red Cross website : backstage of a revamp like
+                    no other
+                  </strong>
+                  <p>
+                    It&lsquo;s a pleasure for the whole team to announce today
+                    the release of the new Belgian Red Cross website. It was an
+                    opportunity for Spade to
+                  </p>
+                  <span>bordet.be</span>
+                </div>
+              </div>
+
+              <Actions>
+                <a href="">
+                  <img src="images/comments.svg" alt="Comments" />
+                  <span>3</span>
+                </a>
+                <a href="">
+                  <img src="images/retweet.svg" alt="Retweet" />
+                  <span>6</span>
+                </a>
+                <a href="">
+                  <img src="images/like.svg" alt="Like" />
+                  <span>2</span>
+                </a>
+                <a href="">
+                  <img src="images/message.svg" alt="Message" />
+                  <span>12</span>
+                </a>
+              </Actions>
+            </Info>
+          </TweetItem>
+        </TweetList>
+      </Content>
+    </Container>
+  );
 }
--- src/components/Timeline/styles.ts
+++ src/components/Timeline/styles.ts
@@ -1,5 +1,152 @@
 import styled from 'styled-components';
+import { Container as Box } from '../Box/styles';
 
 export const Container = styled.section`
   flex: 1;
+  margin: 1rem 1.25rem 0;
+`;
+
+export const Content = styled(Box)``;
+
+export const TweetList = styled.ul`
+  list-style: none;
+`;
+
+export const TweetItem = styled.li`
+  border-bottom: 1px solid var(--gray-200);
+  padding: 0.625rem 1rem;
+  display: flex;
+
+  .retweet {
+    display: flex;
+    align-items: center;
+    font-size: 0.8125rem;
+    color: var(--gray-400);
+    font-weight: normal;
+    margin-bottom: 0.5rem;
+    margin-left: -0.5rem;
+
+    img {
+      width: 0.75rem;
+      height: 0.75rem;
+    }
+
+    span {
+      margin-left: 0.5rem;
+    }
+  }
+`;
+
+export const Avatar = styled.img`
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+`;
+
+export const Info = styled.div`
+  margin-left: 0.625rem;
+  display: flex;
+  flex-direction: column;
+
+  strong {
+    font-size: 0.875rem;
+    color: var(--gray-750);
+
+    span {
+      font-size: 0.8125rem;
+      color: var(--gray-400);
+      font-weight: normal;
+      position: relative;
+
+      & + span {
+        margin-left: 1rem;
+
+        &::before {
+          content: '';
+          width: 0.125rem;
+          height: 0.125rem;
+          border-radius: 50%;
+          background-color: var(--gray-400);
+          position: absolute;
+          left: -4px;
+          top: 6px;
+        }
+      }
+    }
+  }
+
+  p {
+    font-size: 0.875rem;
+    color: var(--gray-750);
+    margin-top: 0.5rem;
+
+    a {
+      text-decoration: none;
+      color: var(--blue-300);
+    }
+  }
+
+  > a {
+    text-decoration: none;
+    color: var(--gray-400);
+    margin-top: 0.5rem;
+  }
+
+  .image {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    > img {
+      height: 320px;
+      width: 320px;
+      padding: 1rem;
+      border: 1px solid var(--gray-200);
+      border-radius: 0.5rem 0.5rem 0 0;
+    }
+
+    div.legend {
+      display: flex;
+      flex-direction: column;
+      border: 1px solid var(--gray-200);
+      padding: 0.5rem;
+
+      strong {
+        font-size: 0.875rem;
+        color: var(--gray-750);
+      }
+
+      p {
+        font-size: 0.875rem;
+        color: var(--gray-750);
+        margin-top: 0.5rem;
+      }
+
+      span {
+        font-size: 0.8125rem;
+        color: var(--gray-400);
+        margin-top: 0.25rem;
+      }
+    }
+  }
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  margin-top: 1rem;
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 0.75rem;
+    color: var(--gray-550);
+
+    span {
+      margin-left: 0.5rem;
+    }
+  }
 `;
